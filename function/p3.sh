32e09536007d36741cfbf36d13df0c9482d89de6
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-12 23:02:15 +0530

    Update p3.sh

--- function/p3.sh
+++ function/p3.sh
@@ -1,3 +1,7 @@
+Take a number from user and check if the number is a Prime then show that its palindrome is also prime
+# a. Write function check if number is Prime
+# b. Write function to get the Palindrome.
+# c. Check if the Palindrome number is also prime
 function prime()
 {
 prime=0

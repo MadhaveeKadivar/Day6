fbe22e0274c3aa20d155fc6a343ccd1eb115ca2e
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-12 23:04:53 +0530

    Update p4.sh

--- whileloop/p4.sh
+++ whileloop/p4.sh
@@ -1,3 +1,5 @@
+# Write a Program where a gambler starts with Rs 100 and places Re 1 bet until he/she goes broke i.e. no more money to gamble or reaches the
+# goal of Rs 200. Keeps track of number of times won and number of bets made.
 rs=100
 totalbets=0
 win=0

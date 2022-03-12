8d9705791861fb3566996bee06c3737b5e37a0d6
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-12 23:00:37 +0530

    Update p1.sh

--- function/p1.sh
+++ function/p1.sh
@@ -1,3 +1,8 @@
+#Help user find degF or degC based on their Conversion Selection. Use Case Statement and ensure that the inputs are within the Freezing Point (
+# 0 °C / 32 °F ) and the Boiling Point of Water ( 100 °C / 212 °F )
+# a. degF = (degC * 9/5) + 32
+# b. degC = (degF – 32) * 5/9
+
 function degF(){
 fah=`echo "scale=4; ($c*(9/5))+32" | bc`
 echo "$c Degree Celsius = $fah Degree Fahrenheit"
